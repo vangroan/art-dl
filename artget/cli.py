733a124836f5401d7c66a4e80db20f90f660d320
Wimpie Victor <wimpie@colourtech.co.za>
2015-07-07 09:01:02 +0200

    Moved command line script

--- artget/cli.py
+++ artget/cli.py
@@ -1,3 +1,49 @@
 
+from argparse import ArgumentParser
+import os, sys
+
+from artget.core import Application
+
+def default_output_directory():
+    return os.path.join(os.environ['USERPROFILE'], 'Downloads', 'test')
+
+def parse_args():
+
+    parser = ArgumentParser(description='A deviantart webscraper')
+
+    parser.add_argument(
+        'galleries',
+        nargs='+',
+        help='Usernames of deviantart galleries'
+    )
+
+    parser.add_argument(
+        '--directory', '-d',
+        dest='output_directory',
+        default=default_output_directory(),
+        help='Output directory'
+    )
+
+    parser.add_argument(
+        '--workers', '-w',
+        dest='workers',
+        default=15
+    )
+
+    parser.add_argument(
+        '--sleep', '-s',
+        dest='sleep',
+        default=0.5
+    )
+
+    return parser.parse_args()
+
 def main():
-    print('Running...')
+    args = parse_args()
+
+    app = Application(args)
+    app.run()
+
+if __name__ == '__main__':
+
+    main()
