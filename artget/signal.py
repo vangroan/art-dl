a101a6cc2bfb93e1ce2b7adacd057758575c1658
Wimpie Victor <wimpie@colourtech.co.za>
2015-07-07 10:46:08 +0200

    Signal unit tests

--- artget/signal.py
+++ artget/signal.py
@@ -1,4 +1,7 @@
 
+class SignalRunningException(Exception): pass
+class CallbackNotFoundException(Exception): pass
+
 class Slot:
 
     def __init__(self, signal, callback):
@@ -41,15 +44,29 @@ class Signal:
 
     def add(self, func):
         slot = Slot(self, func)
-        self._slots.append(func)
+        self._slots.append(slot)
         return slot
 
     def send(self, *args, **kwargs):
+        self._running = True
         for slot in self._slots:
             slot.execute(*args, **kwargs)
+        self._running = False
 
     def remove(self, func):
-        raise NotImplementedError('Todo')
+        filtered = list(filter(lambda el: el.callback == func, self._slots))
 
-    def remove_slot(self, func):
-        raise NotImplementedError('Todo')
+        if len(filtered) == 0:
+            raise CallbackNotFoundException('Callback not found in signal')
+
+        for slot in filtered:
+            self.remove_slot(slot)
+
+    def remove_slot(self, slot):
+        if self.running:
+            raise SignalRunningException('Slot cannot be detached while signalling')
+        self._slots = list(filter(lambda el: el != slot, self._slots))
+        slot.destroy()
+
+    def __len__(self):
+        return len(self._slots)
--- test/test_signal.py
+++ test/test_signal.py
@@ -1,7 +1,100 @@
 
 from unittest import TestCase
+import sys, os
+
+sys.path.append(os.pardir)
+from artget.signal import Signal, Slot, SignalRunningException
 
 class SignallingTest(TestCase):
 
     def setUp(self):
         pass
+
+    def test_add_func_to_signal_via_slot_contents(self):
+        sig = Signal()
+        def go():
+            x = 1 + 2 # Do nothing
+        slot = sig.add(go)
+        self.assertEqual(slot.callback, go)
+
+    def test_signal_sending_messages(self):
+        sig = Signal()
+        count = 0
+        def go():
+            nonlocal count
+            count += 1
+        slot = sig.add(go)
+        [sig.send() for i in range(3)]
+        self.assertEqual(count, 3)
+
+    def test_signal_argument_passing(self):
+        sig = Signal()
+        count = 3
+        def go(val):
+            nonlocal count
+            count += val
+        slot = sig.add(go)
+        sig.send(7)
+        self.assertEqual(count, 10)
+
+    def test_signal_args_and_kwargs(self):
+        sig = Signal()
+        count = 11
+        data = 'foo'
+        def go(val, say='default_string'):
+            nonlocal count, data
+            count += val
+            data = say
+        slot = sig.add(go)
+        sig.send(3, say='bar')
+        self.assertEqual(count, 14)
+        self.assertEqual(data, 'bar')
+
+    def test_slot_removal(self):
+        sig = Signal()
+        count = 3
+        def go(val):
+            nonlocal count
+            count += val
+        slot = sig.add(go)
+        sig.send(5) # Count is now 8
+        sig.remove_slot(slot)
+        sig.send(7) # If count is 15, then removal failed
+        self.assertEqual(count, 8)
+
+    def test_slot_detach(self):
+        sig = Signal()
+        count = 7
+        def go(val):
+            nonlocal count
+            count += val
+        slot = sig.add(go)
+        sig.send(3) # Count is now 10
+        slot.detach()
+        sig.send(7) # If count is 17, then removal failed
+        self.assertEqual(count, 10)
+
+    def test_cannot_remove_slot_while_signalling(self):
+
+        class BadRemove:
+            def __init__(self, signal):
+                self.slot = signal.add(self.do_remove)
+            def do_remove(self):
+                self.slot.detach()
+
+        sig = Signal()
+        bad_remover = BadRemove(sig)
+        with self.assertRaises(SignalRunningException):
+            sig.send()
+
+    def test_remove_function_instance_directly(self):
+        sig = Signal()
+        count = 4
+        def go(val):
+            nonlocal count
+            count += val
+        slot = sig.add(go)
+        sig.send(3) # count is now 7
+        sig.remove(go)
+        sig.send(6) # If count is 13, then removal failed
+        self.assertEqual(count, 7)
