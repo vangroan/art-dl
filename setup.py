3fda85c1623a8657e9f615dc5d4aa7ace3d68b76
Willem Victor <wimpievictor@gmail.com>
2016-01-07 10:29:18 +0200

    Bump version to 0.0.2b

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'artget',
-    version = '0.0.1',
+    version = '0.0.2b',
     packages = ['artget', 'artget.scrapers'],
     install_requires = [
         'aiohttp',
