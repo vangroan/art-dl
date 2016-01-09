c3c34d1cf9ca2eaa06eb2e2c5362c5852d15e5ca
Willem Victor <wimpievictor@gmail.com>
2016-01-09 13:24:34 +0200

    Moving to date based version scheme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'artget',
-    version = '0.0.3b',
+    version = '2016.01.09',
     packages = ['artget', 'artget.scrapers'],
     install_requires = [
         'aiohttp',
