7b68a0062a5cf462919d1d89bbe6a60163ada8cc
Willem Victor <wimpievictor@gmail.com>
2016-02-16 16:28:29 +0200

    Tabs to spaces

--- test/test_rulematch.py
+++ test/test_rulematch.py
@@ -1,4 +1,3 @@
-
 import unittest
 from unittest.mock import Mock
 import sys, os
@@ -9,93 +8,92 @@ from art_dl.rulematch import RegexRule, PatternRules, RuleException
 
 
 class RegexRuleTests(unittest.TestCase):
+    def test_group_split(self):
+        pattern = re.compile(r'(?P<title>[a-z]+)/([0-9]+)')
+        args, kwargs = RegexRule._split_groups(pattern.search('foo/0042'))
 
-	def test_group_split(self):
-		pattern = re.compile(r'(?P<title>[a-z]+)/([0-9]+)')
-		args, kwargs = RegexRule._split_groups(pattern.search('foo/0042'))
-
-		self.assertEqual('0042', args[0])
-		self.assertEqual('foo', kwargs['title'])
+        self.assertEqual('0042', args[0])
+        self.assertEqual('foo', kwargs['title'])
 
 
 class PatternRulesTests(unittest.TestCase):
+    def test_adding_rules(self):
+        rules = PatternRules()
+        rules.add_rule(r'[a-z]', lambda: True)
 
-	def test_adding_rules(self):
-		rules = PatternRules()
-		rules.add_rule(r'[a-z]', lambda: True)
+        self.assertEqual(1, len(rules))
 
-		self.assertEqual(1, len(rules))
+    def test_unhandled_exception(self):
+        rules = PatternRules()
+        rules.add_rule(r'[a-z]', lambda: True)
 
-	def test_unhandled_exception(self):
-		rules = PatternRules()
-		rules.add_rule(r'[a-z]', lambda: True)
+        with self.assertRaises(RuleException):
+            rules.dispatch('123')
 
-		with self.assertRaises(RuleException):
-			rules.dispatch('123')
+    def test_basic_dispatch(self):
+        rules = PatternRules()
+        f = Mock()
+        rules.add_rule(r'http://my\.test\.com/', f)
+        rules.dispatch('http://my.test.com/')
 
-	def test_basic_dispatch(self):
-		rules = PatternRules()
-		f = Mock()
-		rules.add_rule(r'http://my\.test\.com/', f)
-		rules.dispatch('http://my.test.com/')
+        f.assert_any_calls()
 
-		f.assert_any_calls()	
+    def test_dispatch_with_args(self):
+        rules = PatternRules()
+        f = Mock()
+        rules.add_rule(r'^http://my\.test\.com/([a-zA-Z]+)/([0-9]{4})/$', f)
+        rules.dispatch('http://my.test.com/foo/0042/')
 
-	def test_dispatch_with_args(self):
-		rules = PatternRules()
-		f = Mock()
-		rules.add_rule(r'^http://my\.test\.com/([a-zA-Z]+)/([0-9]{4})/$', f)	
-		rules.dispatch('http://my.test.com/foo/0042/')
+        f.assert_called_with('foo', '0042')
 
-		f.assert_called_with('foo', '0042')
+    def test_dispatch_with_kwrags(self):
+        rules = PatternRules()
+        f = Mock()
+        rules.add_rule(r'^http://my\.test\.com/(?P<title>[a-zA-Z]+)/([0-9]{4})/$', f)
+        rules.dispatch('http://my.test.com/foo/0042/')
 
-	def test_dispatch_with_kwrags(self):
-		rules = PatternRules()
-		f = Mock()
-		rules.add_rule(r'^http://my\.test\.com/(?P<title>[a-zA-Z]+)/([0-9]{4})/$', f)	
-		rules.dispatch('http://my.test.com/foo/0042/')
+        f.assert_called_with('0042', title='foo')
 
-		f.assert_called_with('0042', title='foo')
+    def test_inject_context(self):
+        rules = PatternRules()
+        f = Mock()
+        rules.add_rule(r'[a-z]+/([0-9]+)', f, inject_context=True)
+        rules.dispatch('foo/0042')
 
-	def test_inject_context(self):
-		rules = PatternRules()
-		f = Mock()
-		rules.add_rule(r'[a-z]+/([0-9]+)', f, inject_context=True)
-		rules.dispatch('foo/0042')
+        f.assert_called_once_with({
+            'input': 'foo/0042',
+            'pattern': r'[a-z]+/([0-9]+)',
+            'args': ('0042',),
+            'kwargs': {}
+        }, '0042')
 
-		f.assert_called_once_with({
-			'input' : 'foo/0042',
-			'pattern' : r'[a-z]+/([0-9]+)',
-			'args' : ('0042',),
-			'kwargs' : {}
-			}, '0042')
+    def test_dispatch_context_processor(self):
+        rules = PatternRules()
+        value = None
 
-	def test_dispatch_context_processor(self):
-		rules = PatternRules()
-		value = None
+        def processor(ctx):
+            ctx['test_processor'] = 'foobar'
 
-		def processor(ctx):
-			ctx['test_processor'] = 'foobar'
+        def handler(ctx):
+            nonlocal value
+            value = ctx['test_processor']
 
-		def handler(ctx):
-			nonlocal value
-			value = ctx['test_processor']
+        rules.add_rule(r'[a-z]+', handler, inject_context=True)
+        rules.dispatch('foo', context_processor=processor)
 
-		rules.add_rule(r'[a-z]+', handler, inject_context=True)
-		rules.dispatch('foo', context_processor=processor)
+        self.assertEqual(value, 'foobar')
 
-		self.assertEqual(value, 'foobar')
+    def test_dispatch_return(self):
+        rules = PatternRules()
 
-	def test_dispatch_return(self):
-		rules = PatternRules()
+        def factory():
+            return ('foo', 'bar')
 
-		def factory():
-			return ('foo', 'bar')
+        rules.add_rule(r'[a-z]+', factory)
+        result = rules.dispatch('foo')
 
-		rules.add_rule(r'[a-z]+', factory)
-		result = rules.dispatch('foo')
+        self.assertEqual(('foo', 'bar'), result)
 
-		self.assertEqual(('foo', 'bar'), result)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
