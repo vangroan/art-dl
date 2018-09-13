e037edda31ea9563993ef4413eb5b812a7f1eb72
Willem Victor <wimpievictor@gmail.com>
2018-09-13 23:40:33 +0200

    Basic concurrency foundation

--- scrapers/deviantart.go
+++ scrapers/deviantart.go
@@ -1,7 +1,9 @@
 package scrapers
 
 import (
+	"log"
 	"sync"
+	"time"
 
 	artdl "github.com/vangroan/art-dl"
 )
@@ -12,6 +14,8 @@ const (
 
 type DeviantArtScraper struct {
 	baseScraper
+
+	seeds []string
 }
 
 func NewDeviantArtScraper(seedURLs []string, config *artdl.Config) artdl.Scraper {
@@ -19,6 +23,8 @@ func NewDeviantArtScraper(seedURLs []string, config *artdl.Config) artdl.Scraper
 		baseScraper: baseScraper{
 			config: config,
 		},
+
+		seeds: seedURLs,
 	}
 }
 
@@ -28,4 +34,42 @@ func (s *DeviantArtScraper) GetName() string {
 
 func (s *DeviantArtScraper) Run(wg *sync.WaitGroup) {
 	defer wg.Done()
+
+	toFetch := make(chan string, 128)
+	toDownload := make(chan string, 128)
+
+	defer close(toFetch)
+	defer close(toDownload)
+
+	go func() {
+		for i := 0; i < len(s.seeds); i++ {
+			log.Printf("Seeding: %s\n", s.seeds[i])
+			toFetch <- s.seeds[i]
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case url := <-toFetch:
+			s.fetch(url, toDownload)
+		case url := <-toDownload:
+			s.download(url)
+		case t := <-ticker.C:
+			log.Println("Current time: ", t)
+			return
+		}
+		log.Println("Looping...")
+	}
+}
+
+func (s *DeviantArtScraper) fetch(url string, toDownload chan string) {
+	log.Println("Fetching: ", url)
+	toDownload <- url
+}
+
+func (s *DeviantArtScraper) download(url string) {
+	log.Println("Downloading: ", url)
 }
