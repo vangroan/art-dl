572dc10a826bbe5e858fd5c28aaf37102d5b3321
Willem Victor <wimpievictor@gmail.com>
2019-06-06 18:20:45 +0200

    Implemented semphore acquire and release

--- sem.go
+++ sem.go
@@ -21,8 +21,37 @@ func (sem *Semaphores) Add(key string, concurrency int) error {
 	if _, ok := sem.inner[key]; ok {
 		return fmt.Errorf("Key %s already exists", key)
 	} else {
-		sem.inner[key] = make(chan struct{})
+		sem.inner[key] = make(chan struct{}, concurrency)
 	}
 
 	return nil
+}
+
+// Acquire takes a slot from the semaphore associated
+// with the key, blocking the current goroutine if there
+// is no available slot.
+//
+// Returns an error if the key does not exist.
+func (sem *Semaphores) Acquire(key string) error {
+	if semaphore, ok := sem.inner[key]; ok {
+		semaphore <- struct{}{}
+
+		return nil
+	} else {
+		return fmt.Errorf("Key %s does not exist", key)
+	}
+}
+
+// Release frees up a slot at the given semaphore, allowing
+// other goroutines to resume execution.
+//
+// Returns an error if the key does not exist.
+func (sem *Semaphores) Release(key string) error {
+	if semaphore, ok := sem.inner[key]; ok {
+		<-semaphore
+
+		return nil
+	} else {
+		return fmt.Errorf("Key %s does not exist", key)
+	}
 }
