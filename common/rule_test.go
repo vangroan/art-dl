3d261fb3717cb183fdd97211d7d6c4d5609e8f12
Willem Victor <wimpievictor@gmail.com>
2019-07-19 14:02:40 +0200

    Fixed test

--- common/rule_test.go
+++ common/rule_test.go
@@ -1,24 +1,43 @@
 package common
 
 import (
+	"fmt"
 	"sync"
 	"testing"
 )
 
-type noopScraper struct{}
+type noopScraper struct {
+	runCallback func(matches []RuleMatch)
+}
 
 func (s *noopScraper) GetName() string { return "NoOp" }
 
-func (s *noopScraper) Run(wg *sync.WaitGroup) error {
+func (s *noopScraper) Run(wg *sync.WaitGroup, matches []RuleMatch) error {
+	s.runCallback(matches)
+
 	return nil
+}
+
+type result struct {
+	ID    int
+	Match RuleMatch
 }
 
 func TestResolve(t *testing.T) {
 	// Arrange
-	results := make([]RuleMatch, 0)
-	var f RuleFactoryFunc = func(matches []RuleMatch, config *Config) Scraper {
-		results = append(results, matches...)
-		return &noopScraper{}
+	results := make([]result, 0)
+	var f RuleFactoryFunc = func(id int, config *Config) Scraper {
+		return &noopScraper{
+			runCallback: func(matches []RuleMatch) {
+				for _, match := range matches {
+					fmt.Println(id)
+					results = append(results, result{
+						ID:    id,
+						Match: match,
+					})
+				}
+			},
+		}
 	}
 	resolver := NewRuleResolver()
 	resolver.SetMappings(
@@ -34,19 +53,33 @@ func TestResolve(t *testing.T) {
 	// Act
 	scrapers := resolver.Resolve(urls)
 
+	for _, entry := range scrapers {
+		entry.Scraper.Run(nil, entry.Seeds)
+	}
+
 	// Assert
 	if len(scrapers) != 2 {
 		t.Fatalf("Expected %d, actual %d", 2, len(scrapers))
 	}
 
-	assert := func(expected, actual string) {
+	assertInt := func(expected, actual int) {
+		if expected != actual {
+			t.Fatalf("Expected %d, actual %d", expected, actual)
+		}
+	}
+
+	assertStr := func(expected, actual string) {
 		if expected != actual {
 			t.Fatalf("Expected %s, actual %s", expected, actual)
 		}
 	}
-	expected := []string{"one", "two", "three"}
 
-	for idx, match := range results {
-		assert(expected[idx], match.UserInfo)
-	}
+	// deviantart
+	assertInt(1, results[0].ID)
+	assertStr("one", results[0].Match.UserInfo)
+	assertStr("two", results[1].Match.UserInfo)
+
+	// artstation
+	assertInt(2, results[2].ID)
+	assertStr("three", results[2].Match.UserInfo)
 }
