71b71d93212dfe276de52df5d809685288878f16
Willem Victor <wvictor@retrorabbit.co.za>
2019-07-21 19:13:56 +0200

    Added boilerplate code for Art Station

---
+++ scrapers/artstation.go
@@ -0,0 +1,33 @@
+package scrapers
+
+import (
+	"sync"
+
+	artdl "github.com/vangroan/art-dl/common"
+)
+
+// ArtStationScraper scrapers gallerie on artstation.com
+type ArtStationScraper struct {
+	baseScraper
+}
+
+// NewArtStationScraper creates a new deviantart scraper
+func NewArtStationScraper(id int, config *artdl.Config) artdl.Scraper {
+	return &ArtStationScraper{
+		baseScraper: baseScraper{
+			id:     id,
+			config: config,
+		},
+	}
+}
+
+// GetName returns a descriptive name for the scraper.
+func (s *ArtStationScraper) GetName() string {
+	return "ArtStation"
+}
+
+// Run starts the scraper
+func (s *ArtStationScraper) Run(wg *sync.WaitGroup, matches []artdl.RuleMatch) error {
+
+	return nil
+}
